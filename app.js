3b5f8b2ca97db2fa01381cccb44250b0f6cbb094
gabrielatomaz <32981034+gabrielatomaz@users.noreply.github.com>
2020-02-06 09:35:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser')
 const path = require('path');
 const cors = require('cors');
 const { loginScrapper } = require("./services/w-scraper");
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
