f630549705fe60466ca54e8132d45514353a404e
Gabriela Ribeiro <gabriela.ribeiro@zenvia.com>
2020-11-09 11:43:41 -0300

    chagend commentary

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 
 routes.get('/', UserController.indexLogin);
 routes.get('/index', UserController.index);
-//account = { agency, number, password };
+//body = { agency, number, password };
 routes.post('/api/login', UserController.login);
 routes.post('/api/logout', UserController.logout);
 routes.get('/api/infos', UserController.listAllInfos);
